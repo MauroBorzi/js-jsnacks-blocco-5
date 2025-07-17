de4633ddb618a1ad749b5609845891c09d382251
MauroBorzi <mauroborzi91@gmail.com>
2025-07-17 15:09:17 +0200

    [add]snack-3

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,13 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
+
+const newNumbers = []
+
+numbers.map((number) => {
+  newNumbers.push(number + 1)
+})
+
+console.log(newNumbers)
+
 // Risultato: [3, 9, 5, 8, 3, 88]
