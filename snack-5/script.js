4ceed726e6ef029bca72ebd96580166e14001315
MauroBorzi <mauroborzi91@gmail.com>
2025-07-17 15:15:52 +0200

    [add]snack-5

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,15 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
+
+const newNums = []
+
+nums.filter((num) => {
+  if (num % 2 === 0) {
+    newNums.push(num)
+  }
+})
+
+console.log(newNums)
+
 // Risultato: [2, 8, 4, 12]
